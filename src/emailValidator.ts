9f93e16cb54ffc82883fb5bbfd6d4d9e952efc47
walid-nouamani-vinci <walid.nouamani@student.vinci.be>
2026-01-16 17:15:36 +0100

    add isValidEmail

---
+++ src/emailValidator.ts
@@ -0,0 +1,6 @@
+export class emailValidator {
+
+    isValidEmail(): boolean {
+        return true;
+    }
+}
---
+++ tests/emailValidator.test.ts
@@ -0,0 +1,11 @@
+import { emailValidator } from "../src/emailValidator";
+
+describe("Email validation", () => {
+    it("should return true when given email", () => {
+        const emailValidation = new emailValidator();
+
+        const actual = emailValidation.isValidEmail();
+
+        expect (actual).toBe(true);
+    })
+});
