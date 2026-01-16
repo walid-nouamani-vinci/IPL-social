ef8d3c6db25ce742ed6e6042e336f90cda032bcd
walid-nouamani-vinci <walid.nouamani@student.vinci.be>
2026-01-16 17:21:23 +0100

    add isValidEmail without @

--- src/emailValidator.ts
+++ src/emailValidator.ts
@@ -1,6 +1,8 @@
 export class emailValidator {
 
-    isValidEmail(): boolean {
+    isValidEmail(email: string): boolean {
+        const atIndex = email.indexOf("@");
+        if (atIndex <= 0) return false;
         return true;
     }
 }
--- tests/emailValidator.test.ts
+++ tests/emailValidator.test.ts
@@ -4,8 +4,16 @@ describe("Email validation", () => {
     it("should return true when given email", () => {
         const emailValidation = new emailValidator();
 
-        const actual = emailValidation.isValidEmail();
+        const actual = emailValidation.isValidEmail("test@email.com");
 
         expect (actual).toBe(true);
+    })
+
+    it("should return false when given email without @", () => {
+        const emailValidation = new emailValidator();
+
+        const actual = emailValidation.isValidEmail("testemail.com");
+
+        expect (actual).toBe(false);
     })
 });
